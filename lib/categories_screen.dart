581e632edce46f9c4e1f0db8a5b535d734c52f76
Furkan Ayyıldız <furkan199905@gmail.com>
2022-12-12 12:53:23 +0300

    category data is sended into category item

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import "./dummy_data.dart";
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -6,8 +9,15 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200, //* width
         childAspectRatio: 3 / 2, //* heigt in width e oranı
         crossAxisSpacing: 20,
