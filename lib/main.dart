5614dc68bb282e95fd13574d3585a49774133350
Furkan Ayyıldız <furkan199905@gmail.com>
2022-12-12 16:16:54 +0300

    navigation to category meals screen with pushNamed propertie

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // page imports
 import "./categories_screen.dart";
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,6 +31,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        "/category-meals": (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
