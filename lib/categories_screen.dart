99207c511de67c525a41bba0bb6cd988840174c8
Furkan Ayyıldız <furkan199905@gmail.com>
2022-12-12 13:10:08 +0300

    category screen passed to home at main

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,20 +8,25 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(
-              catData.title,
-              catData.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200, //* width
-        childAspectRatio: 3 / 2, //* heigt in width e oranı
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ozay Lokanta"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, //* width
+          childAspectRatio: 3 / 2, //* heigt in width e oranı
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
